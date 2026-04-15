9634a86243007948a8e8ad81eda8da378a4e706e
Sailaxmi V <122695930+Sailaxmiveldanda@users.noreply.github.com>
2026-04-15 10:40:37 -0700

    Time: 0 ms (100.00%) | Memory: 45.6 MB (99.35%) - LeetSync

--- 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.java
+++ 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.java
@@ -1,14 +1,26 @@
 class Solution {
     public int closestTarget(String[] words, String target, int startIndex) {
-        int n =  words.length;
-        for(int i = 0; i < n ; i++){
-            int left = (startIndex + i) % n;
-            int right = (startIndex - i + n) % n;
+        int n = words.length;
+        int mdist = Integer.MAX_VALUE;
 
-            if(words[left].equals(target) || words[right].equals(target)){
-                return i;
+        for(int i = 0; i < n ; i++){
+            if(words[i].equals(target)){
+                int diff = Math.abs(i - startIndex);
+                int dist = Math.min(diff, n - diff);
+                mdist = Math.min(mdist,dist);
             }
         }
-        return -1;
+        return mdist == Integer.MAX_VALUE ? -1 : mdist;
     }
 }
+
+//  int n =  words.length;
+//         for(int i = 0; i < n ; i++){
+//             int left = (startIndex + i) % n;
+//             int right = (startIndex - i + n) % n;
+
+//             if(words[left].equals(target) || words[right].equals(target)){
+//                 return i;
+//             }
+//         }
+//         return -1;
