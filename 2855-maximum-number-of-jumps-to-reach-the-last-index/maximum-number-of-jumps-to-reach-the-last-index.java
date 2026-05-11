255ac488065b3dfc546b753dad30841894164c02
Sailaxmi V <122695930+Sailaxmiveldanda@users.noreply.github.com>
2026-05-11 10:25:18 -0700

    Time: 16 ms (65.27%) | Memory: 46.9 MB (74.85%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.java
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.java
@@ -4,14 +4,13 @@ class Solution {
         int[] dp = new int[n];
         Arrays.fill(dp, -1);
         dp[0] = 0;
-        for(int i = 1; i < n; i++) {
-            for(int j = 0; j < i; j++) {
-                if(Math.abs(nums[i] - nums[j]) <= target && dp[j] != -1) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(Math.abs(nums[i] - nums[j]) <= target && dp[j] != -1){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
                 }
             }
         }
-
-        return dp[n - 1];
+        return dp[n-1];
     }
 }
