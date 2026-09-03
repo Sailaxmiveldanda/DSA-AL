f4eb5dded1153e570e30603f5b8313e65843e0b2
Sailaxmi V <122695930+Sailaxmiveldanda@users.noreply.github.com>
2026-09-02 18:20:56 -0700

    Time: 2 ms (99.65%) | Memory: 44.7 MB (54.61%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,18 +1,31 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
-        }
-        HashMap<Character,Integer> map1 = new HashMap<>();
-        HashMap<Character,Integer> map2 = new HashMap<>();
+        // if(s.length() != t.length()){
+        //     return false;
+        // }
+        // HashMap<Character,Integer> map1 = new HashMap<>();
+        // HashMap<Character,Integer> map2 = new HashMap<>();
+
+        // for(char ch : s.toCharArray()){
+        //     map1.put(ch, map1.getOrDefault(ch , 0) +1);
+        // }
+        // for(char ch : t.toCharArray()){
+        //     map2.put(ch, map2.getOrDefault(ch,0) +1);
+        // }
+        // return map1.equals(map2);
+        // //time - O(n) space - O(k)
+
+        //freq method
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
 
         for(char ch : s.toCharArray()){
-            map1.put(ch, map1.getOrDefault(ch , 0) +1);
+            freq1[ch - 'a']++;
         }
         for(char ch : t.toCharArray()){
-            map2.put(ch, map2.getOrDefault(ch,0) +1);
+            freq2[ch - 'a']++;
         }
-        return map1.equals(map2);
-        //time - O(n) space - O(k)
+        return Arrays.equals(freq1,freq2);
+        //time - O(n) space O(1)
     }
 }
