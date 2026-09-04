8f471c862ca0086d4470430927cbc61ba9d65289
Sailaxmi V <122695930+Sailaxmiveldanda@users.noreply.github.com>
2026-09-03 18:23:23 -0700

    Time: 1 ms (99.94%) | Memory: 77.1 MB (66.39%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,11 +1,12 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int currsum = nums[0];
-        int maxsum = nums[0];
-        for(int i = 1; i < nums.length; i++){
-            currsum = Math.max(currsum + nums[i], nums[i]);
-            maxsum = Math.max(maxsum,currsum);
+        //Kadane Algorithm
+        int curr = nums[0];
+        int max = nums[0];
+        for(int i = 1; i < nums.length ; i++){
+            curr = Math.max(curr + nums[i], nums[i]);
+            max = Math.max(max,curr);
         }
-        return maxsum;
+        return max;
     }
 }
