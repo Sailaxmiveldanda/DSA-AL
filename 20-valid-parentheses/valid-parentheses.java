01e0d2262c925182aaf92e33ebf9984b54c72f54
Sailaxmi V <122695930+Sailaxmiveldanda@users.noreply.github.com>
2026-09-21 19:20:41 -0700

    Time: 4 ms (36.74%) | Memory: 43.1 MB (85.44%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,19 +1,17 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
+        Deque<Character> stack = new ArrayDeque<>();
+        Map<Character,Character> map = new HashMap<>();
+        map.put('}','{');
+        map.put(')','(');
+        map.put(']','[');
         for(char ch : s.toCharArray()){
-            if(ch == '(' || ch == '[' || ch == '{'){
+            if(!map.containsKey(ch)){
                 stack.push(ch);
             }
             else{
-                if(stack.isEmpty()){
+                if(stack.isEmpty() || stack.pop() != map.get(ch)){
                     return false;
-                }
-                else{
-                    char top = stack.pop();
-                    if((top != '(' && ch == ')') || (top != '{' && ch == '}') || (top != '[' && ch == ']')){
-                        return false;
-                    }
                 }
             }
         }
