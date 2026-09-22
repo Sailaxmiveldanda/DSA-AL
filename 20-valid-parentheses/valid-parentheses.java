2ccc98b473ebdf1e68ece67223b25980b738562d
Sailaxmi V <122695930+Sailaxmiveldanda@users.noreply.github.com>
2026-09-21 19:14:15 -0700

    Time: 2 ms (97.97%) | Memory: 43.3 MB (57.46%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,36 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-        // Stack<Character> stack = new Stack<>();
-        // for(char ch : s.toCharArray()){
-        //     if(ch == '(' || ch == '{' || ch == '['){
-        //         stack.push(ch);
-        //     }
-        //     else{
-        //         if(stack.isEmpty()) return false;
-        //         else{
-        //             char top = stack.pop();
-        //             if((top != '(' && ch == ')') ||
-        //             (top != '[' && ch == ']') ||
-        //             (top != '{' && ch == '}')
-        //             ){
-        //                 return false;
-        //             }
-        //         }
-        //     }
-        // }
-        // return stack.isEmpty();
-        Deque<Character> stack = new ArrayDeque<>();
-        Map<Character, Character> map = new HashMap<>();
-        map.put('}','{');
-        map.put(']','[');
-        map.put(')','(');
+        Stack<Character> stack = new Stack<>();
         for(char ch : s.toCharArray()){
-            if(!map.containsKey(ch)){
+            if(ch == '(' || ch == '[' || ch == '{'){
                 stack.push(ch);
             }
             else{
-                if(stack.isEmpty() || stack.pop() != map.get(ch)){
+                if(stack.isEmpty()){
                     return false;
+                }
+                else{
+                    char top = stack.pop();
+                    if((top != '(' && ch == ')') || (top != '{' && ch == '}') || (top != '[' && ch == ']')){
+                        return false;
+                    }
                 }
             }
         }
